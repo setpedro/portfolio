5bde59479787ccbef3b005bf496278ed63967cf3
setPedro <pedross4789@gmail.com>
2024-10-13 08:55:47 +0200

    add skills info

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -15,6 +15,10 @@ export default function Skills() {
   return (
     <div className="flex flex-col gap-6 items-center">
       <h1 className="text-3xl font-bold">Skills</h1>
+      <div className="flex flex-col gap-2 items-center">
+        <p className="text-center px-2">Not restricted to a specific tech stack and capable of quickly learning and adapting to new tools.</p>
+        <p>Proficient in:</p>
+      </div>
       <div className="grid sm:grid-cols-2 xl:grid-cols-4 gap-4">
         {skills.map((skill, i) => (
           <div key={i}>
